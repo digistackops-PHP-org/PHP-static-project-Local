6b4a0348e9aa9f2ee96bef74c191bf01ff51b565
venkatesh peta <85110131+DigiStackOps@users.noreply.github.com>
2025-08-19 09:26:41 +0530

    Update index.php

--- public/index.php
+++ public/index.php
@@ -17,7 +17,7 @@ $ip = gethostbyname($hostname);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP SPA Project</title>
+    <title>DigiStack PHP Project</title>
     <style>
         body, html {
             height: 100%;
